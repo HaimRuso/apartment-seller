007e60d54099855741c59f6df3d41b486c789fc2
Haim Ruso <haimruso23@gmail.com>
2019-11-27 10:06:36 +0200

    handele bar project

--- apt-filter.js
+++ apt-filter.js
@@ -2,7 +2,7 @@
 
 const must = (radio) => radio.filter(rb => rb.checked).length
 
-const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate) {
+const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate,parking) {
     let relevantApts = apartments.filter(a =>
         a.address.toLowerCase().includes(address.toLowerCase() || "") &&
         a.price >= (minPrice || 0) &&
@@ -10,7 +10,20 @@ const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRoo
         a.rooms >= (minRooms || 0) &&
         a.rooms <= (maxRooms || a.rooms)
     )
-
+    
     immediate = !immediate[0].checked
-    return immediate ? relevantApts : relevantApts.filter(a => (a.immediate)) 
+    parking=!parking[0].checked
+    
+    if(immediate&&parking){
+    return  relevantApts
+    }
+    else if(immediate&&!parking){
+     return relevantApts.filter(p => (p.parking)) 
+    }
+    else if(!immediate&&parking){
+        return relevantApts.filter(i => (i.immediate)) 
+    }
+    else{
+     return relevantApts.filter(p => (p.parking)&&(p.immediate)) 
+    }
 }
--- main.js
+++ main.js
@@ -6,8 +6,9 @@ $("button").on("click", function () {
     let minRooms = $("#min-r-input").val()
     let maxRooms = $("#max-r-input").val()
     let immediate = $("#immed-y")
+    let parking=$('#park-y')
 
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate,parking)
     renderApts(relevantApts)
 })
 
@@ -15,6 +16,10 @@ const renderApts = function (apartments) {
     $("#results").empty()
     console.log(apartments) //array of apartments to render
     //Your code goes here.
+    const source = $('.insert').html();
+    const template = Handlebars.compile(source);
+    let newHTML = template({apartments});
+    $('#results').append(newHTML);  
 }
 
 renderApts(apartments) //renders apartments when page loads
